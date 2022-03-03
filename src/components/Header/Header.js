3b672058083a8ddf4c98fc91d48de6bb4a5f54aa
t-devl <79603858+t-devl@users.noreply.github.com>
2022-03-03 09:49:07 +0000

    Remove button from inside link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,17 +10,15 @@ export default function Header({ basketQuantity }) {
       <Link className="header__link" to="/">
         <div className="header__logo">Commerce</div>
       </Link>
-      <Link className="header__link" to="/basket">
-        <button className="header__basket-button">
-          <FontAwesomeIcon
-            className="header__basket-icon"
-            icon={faBasketShopping}
-          />
-          Basket
-          {basketQuantity ? (
-            <span className="header__basket-quantity">{basketQuantity}</span>
-          ) : null}
-        </button>
+      <Link className="header__basket-button" to="/basket">
+        <FontAwesomeIcon
+          className="header__basket-icon"
+          icon={faBasketShopping}
+        />
+        Basket
+        {basketQuantity ? (
+          <span className="header__basket-quantity">{basketQuantity}</span>
+        ) : null}
       </Link>
     </header>
   );
